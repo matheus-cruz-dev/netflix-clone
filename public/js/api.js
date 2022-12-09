24877a317b541d980491dc544981ed80667c727b
matheus-cruz-dev <matheus.cruz.2004@hotmail.com>
2022-12-09 18:55:39 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "959cdc2990ba95420e08965c50d829d9"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
